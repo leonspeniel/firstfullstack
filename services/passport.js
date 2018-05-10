31d9410f8972f9ae10469fb9bc8329ba6c5eae54
Leons Peniel <leonsp@enqos.com>
2018-05-10 21:23:12 +0530

    refactor

--- services/passport.js
+++ services/passport.js
@@ -1,23 +1,14 @@
-const express = require('express');
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
-
-const app = express();
 
 passport.use(new googleStrategy({
         clientID: keys.googleClientId,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
+    },
+    (accessToken, refreshToken, profile, done) => {
         console.log(accessToken);
+        console.log(profile);
     })
 );
-
-app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
