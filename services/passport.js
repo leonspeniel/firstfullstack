5d962a9d52338eff2b84ac5c03ea18319dca7b44
Leons Peniel <leonsp@enqos.com>
2018-05-11 22:34:14 +0530

    proxy true

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.deserializeUser((id, done) =>{
 passport.use(new googleStrategy({
         clientID: keys.googleClientId,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
 
