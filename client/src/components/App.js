016a41a08b5656a01878b728155a590905052f5a
Leons Peniel <leonsp@enqos.com>
2018-05-15 22:21:57 +0530

    starting with redux

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+
+
+const App = () =>{
+    return(
+        <div>
+            Hello Leons.. This is fullstack speaking..Checking..
+        </div>
+    );
+};
+
+export default App;
