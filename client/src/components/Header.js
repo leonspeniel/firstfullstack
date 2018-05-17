2cf0d3b540cce3958a2ff845770e0a631d2ff58e
Leons Peniel <leonsp@enqos.com>
2018-05-17 19:44:18 +0530

    few redirect on login and logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 
-class Header extends Component{
+class Header extends Component {
 
-    renderContent(){
+    renderContent() {
         switch (this.props.auth) {
 
             case null:
@@ -15,11 +16,15 @@ class Header extends Component{
                 return (<li><a href="/api/logout">LogOut</a></li>);
         }
     }
-    render(){
-        return(
+
+    render() {
+        return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">FirstFullStack</a>
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >Logo</Link>
                     <ul className="right">
                         <li><a>{this.renderContent()}</a></li>
                     </ul>
@@ -28,12 +33,13 @@ class Header extends Component{
         );
     }
 }
+
 /*function mapStateToProps(state) {
     return{auth:state.auth};
 }*/
 
 function mapStateToProps({auth}) {
-    return{auth};
+    return {auth};
 }
 
-export default connect (mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+const Landing = () =>{
+
+    return(
+        <div style={{textAlign : 'center'}}>
+            <h1>
+                Survey Service
+            </h1>
+            Collect feedback from users
+        </div>
+    );
+};
+
+export default Landing;
